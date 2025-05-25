d1283e03f627c34770e689cfecd02ac560c51cdc
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-05-25 10:00:24 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,16 @@
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return jsonify("message": "online")
+	return jsonify({"message": "online"})
+
+@app.route("kivy/<key>")
+def kivy(key):
+	return jsonify({f"{key}": {"temperature": random.randint(25, 40),
+				   "brightness": random.randint(0, 100)})
 
 
 #app.run(debug=True)
